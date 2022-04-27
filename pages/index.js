354bd43b0a084c26ef1723a34e192be5dd47f1e7
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2022-04-27 03:41:57 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,10 @@ import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import data from "./../data/new.json";
 
-export async function getServerSideProps(context) {
-      return {
-        props: {
-          data1: data,
-        }, // will be passed to the page component as props
-      };
-    }
-    
-export default function Home({ data1 }) {
-  console.log(data1);
+
+
+export default function Home() {
+
   const uploadToServer = async () => {
     // const body = new FormData();
     // // console.log("file", image)
@@ -35,7 +29,7 @@ export default function Home({ data1 }) {
       >
         Send to server
       </button>
-      {data1?.map((item, index) => (
+      {data.map((item, index) => (
         <div key={index}>
           <h1>{item.name}</h1>
         </div>
