4d599ad5c4f1d1a8ab074dea2fc6198ac18c8e40
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2022-04-27 03:39:15 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import data from './../data/new.json'
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect } from "react";
+import styles from "../styles/Home.module.css";
+import data from "./../data/new.json";
 
-export default function Home(data1) {
-  const uploadToServer = async () => {        
+export async function getServerSideProps(context) {
+      return {
+        props: {
+          data1: data,
+        }, // will be passed to the page component as props
+      };
+    }
+    
+export default function Home({ data1 }) {
+  console.log(data1);
+  const uploadToServer = async () => {
     // const body = new FormData();
     // // console.log("file", image)
-    // body.append("file", image);    
-    const response = await fetch("/api/add")
+    // body.append("file", image);
+    const response = await fetch("/api/add");
   };
-  console.log(data)
- 
+
+
+   
+
+
 
   return (
     <div>
-       <button
-          className="btn btn-primary"
+      <button
+        className="btn btn-primary"
         //   type="submit"
-          onClick={uploadToServer}
-        >
-          Send to server
-        </button>
-        {data1.map((item, index) => (
-          <div key={index}>
-            <h1>{item.name}</h1>
-            </div>
-        ))}
-
+        onClick={uploadToServer}
+      >
+        Send to server
+      </button>
+      {data1?.map((item, index) => (
+        <div key={index}>
+          <h1>{item.name}</h1>
+        </div>
+      ))}
     </div>
-  )
-}
-
-export async function getServerSideProps(context) {
-  return {
-    props: {
-      data: data1
-    }, // will be passed to the page component as props
-  }
+  );
 }
